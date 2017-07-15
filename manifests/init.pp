6630fef2ea37127dcfe001b01a0ad0e757c95a74
Les Aker <me@lesaker.org>
2017-07-15 15:02:04 -0400

    fix cops

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,11 +2,11 @@
 # Sets up Unifi control panel
 class unifi(
 ) {
-    package { 'unifi':
-        ensure => latest
-    } ->
-    service { 'unifi':
-        enable => true,
-        ensure => running
-    }
+  package { 'unifi':
+    ensure => latest
+  }
+  -> service { 'unifi':
+    ensure => running,
+    enable => true
+  }
 }
